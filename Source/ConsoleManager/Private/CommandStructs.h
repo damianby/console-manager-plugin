92a0eacaffd81f8a54342eff31948e0e27b71211
damian_baldyga <baldygadamian@gmail.com>
2021-01-13 18:48:22 +0100

    Add struct representing command which is being displayed

--- Source/ConsoleManager/Private/CommandStructs.h
+++ Source/ConsoleManager/Private/CommandStructs.h
@@ -7,3 +7,21 @@ struct FCommandGroup
 	FString Name;
 	TArray<FString> Commands;
 };
+
+enum class EConsoleCommandInputType : uint8
+{
+	None,
+	Value,
+	Slider
+};
+
+struct FConsoleCommand
+{
+	FString Name;
+	EConsoleCommandInputType InputType;
+	bool IsValid;
+	FString Command;
+	FString Value;
+	FString Type;
+	FString SetBy;
+};
